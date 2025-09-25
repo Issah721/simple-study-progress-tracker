80e39ec1729556a950a6cbebc83cac9cdbb7c914
Issah721 <johnymuthash96@gmail.com>
2025-09-25 13:00:55 +0300

    added a simple progress code that stores progress in a file

--- progress.py
+++ progress.py
@@ -3,4 +3,9 @@
 print("Welcome to Progress tracker, Ensure to log your progress daily!")
 progress_log = input("Enter your progress for today: ")
 
+print("Logging your progress...")
+with open("progress.txt", "a") as file:
+    file.write(progress_log + "\n")
 
+print("Progress logged successfully!")
+print(f"Here is your progress so far: {open('progress.txt').read()}")
